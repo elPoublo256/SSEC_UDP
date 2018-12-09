9a1af207c8f173d361692bc3fe8ba47f85324c4b
elPoublo256 <elpoublo@gmail.com>
2018-12-09 18:59:21 +0300

    constructor process 1

--- include/udp_process.hpp
+++ include/udp_process.hpp
@@ -1,13 +1,18 @@
 #pragma once
-//#include </usr/include/boost/asio.hpp>
-///this is RAII class which using udp sockets
+#include <boost/asio.hpp>
+#include <boost/asio.hpp>
+#include <boost/bind.hpp>
+#include <boost/date_time/posix_time/posix_time.hpp>
+
+
 #define UDP_PORT 23456
 #define MAX_L 20
 #define MIN_L 5
 #include <string>
 #include <fstream>
-
-
+#include <memory>
+#include <boost/array.hpp>
+using namespace boost::asio;
 class Massage
 {
 public:
@@ -20,17 +25,53 @@ public:
 
 
 };
-
-class Process1
-{
-};
-
-class Process2
+class UDP_Process
 {
 public:
+   const static std::string service_name;// = "ssc_udp_service";
+    virtual void start_working();
+protected:
+
+   std::unique_ptr<io_service> io;
+   ip::udp::endpoint ep;//copyble, heve defult constructors
+   std::unique_ptr<ip::udp::socket> socket;
+   //TODO use boost log lib
+    virtual std::basic_ostream<char>& logger();
+    virtual ~UDP_Process(){}
+   virtual void start(){}
+
+};
+///wait packet from client
+/// it open UDP socket with UDP_PORT
+/// and wait strings from server
+class Process1 : public UDP_Process
+{
+public:
+    std::size_t allMessSize;
+    Process1(const std::string& hostname = std::string("0.0.0.0"));
+    std::string buf_str;
+    std::unique_ptr<ip::udp::resolver> resolver;
+    std::unique_ptr<ip::udp::resolver::query> query;
+    std::unique_ptr<deadline_timer> timer;
+    ~Process1() override;
+    void start() override;
+
+     static void hendler(const boost::system::error_code& err,
+                  std::size_t num_bytes)
+     {}
+
+};
+///print random string with char [a-z]
+/// send string with length = [5-20] bytes
+/// in loop with interval seted in emit constructor
+class Process2 : public UDP_Process
+{
+public:
+    Process2(const unsigned int& interval);
     unsigned int time_interval;
     std::ofstream fileStream;
     std::string mass;
-    //void print_mass;
 
+
+    void send_mess();
 };
--- src/udp_process.cpp
+++ src/udp_process.cpp
@@ -1,5 +1,6 @@
 #include "../include/udp_process.hpp"
 #include <cstdlib>
+
 Massage::Massage()
 {
  length_line = MIN_L + (std::rand() % (MAX_L - MIN_L));
@@ -10,4 +11,46 @@ Massage::Massage()
  }
 }
 
+void UDP_Process::start_working(){}
+const std::string UDP_Process::service_name = "ssc_udp_service";
 
+std::basic_ostream<char>& UDP_Process::logger()
+{
+    return std::cerr;
+}
+
+Process1::Process1(const std::string& hostname)
+{
+    io = std::make_unique<io_service>();
+    resolver = std::make_unique<boost::asio::ip::udp::resolver>(*io);
+    socket = std::make_unique<ip::udp::socket>(*io);
+    query = std::make_unique<ip::udp::resolver::query>
+            (ip::udp::v4(), hostname, this->service_name);
+    ep = *(resolver->resolve(*query));
+}
+
+void Process1::start()
+{
+    try
+    {
+        auto _hendler = [](const boost::system::error_code& err,
+                std::size_t num_bytes){};
+        auto boost_buf = boost::asio::buffer(buf_str);
+    }
+    catch(std::exception &e)
+    {
+        this->logger() << e.what() << std::endl;
+    }
+}
+
+
+
+Process1::~Process1()
+{
+
+}
+
+Process2::Process2(const unsigned int &interval)
+{
+
+}
